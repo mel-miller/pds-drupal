8b02f6e091be325558aac43c69f54b16ee90d552
M Miller <miller.2676@gmail.com>
2022-02-14 15:19:36 -0800

    Published from 'mel-miller/pds-twig'

--- js/pds-main.js
+++ js/pds-main.js
@@ -4,9 +4,9 @@ Drupal.behaviors.modal = {
 
 		modals.forEach((modal) => {
 			const modalId = modal.id;
-			const modalNum = modalId.substring(modalId.indexOf("modal_") + 6);
-			const openBtn = document.getElementById(`modalBtn_${modalNum}`);
-			const currentModal = document.getElementById(`modal_${modalNum}`);
+			const modalNum = modalId.substring(modalId.indexOf("modal--") + 7);
+			const openBtn = document.getElementById(`modalBtn--${modalNum}`);
+			const currentModal = document.getElementById(`modal--${modalNum}`);
 			const closeBtn = currentModal.querySelector(".pds-close-btn");
 
 			// Open modal via open button.
@@ -39,5 +39,118 @@ Drupal.behaviors.modal = {
 				}
 			};
 		});
+	},
+};
+
+Drupal.behaviors.tabset = {
+	attach(context) {
+		const tabsetClass = "pds-tabset";
+
+		const keys = {
+			left: "ArrowLeft",
+			right: "ArrowRight",
+			home: "Home",
+			end: "End",
+		};
+
+		let tabsets = document.querySelectorAll(`.${tabsetClass}`);
+		tabsets = Array.prototype.slice.call(tabsets);
+
+		tabsets.forEach((tabset) => {
+			let tabs = tabset.querySelectorAll(`.${tabsetClass}__tab`);
+			tabs = Array.prototype.slice.call(tabs);
+
+			let panels = tabset.querySelectorAll(`.${tabsetClass}__panel`);
+			panels = Array.prototype.slice.call(panels);
+
+			tabs.forEach((tab) => {
+				tab.addEventListener("keydown", handleKeys);
+
+				tab.addEventListener("click", (event) => {
+					const { currentTarget: currentTab } = event;
+					const otherTabs = tabs.filter((element) => element !== currentTab);
+
+					currentTab.removeAttribute("tabindex");
+					currentTab.setAttribute("aria-selected", true);
+					otherTabs.forEach((otherTab) => {
+						otherTab.setAttribute("tabindex", "-1");
+						otherTab.setAttribute("aria-selected", false);
+					});
+
+					const currentPanelId = currentTab.getAttribute("aria-controls");
+					const currentPanel = document.querySelector(`#${currentPanelId}`);
+					const otherPanels = panels.filter((element) => element !== currentPanel);
+
+					currentPanel.removeAttribute("hidden");
+					otherPanels.forEach((otherPanel) => {
+						otherPanel.setAttribute("hidden", "hidden");
+					});
+				});
+			});
+		});
+
+		function handleKeys(event) {
+			let delay = false;
+
+			const { key: currentKey, currentTarget: currentTab } = event;
+
+			const {
+				parentNode: { parentNode: currentTabpanel, children: tabs },
+				previousElementSibling: previousElement,
+				nextElementSibling: nextElement,
+			} = currentTab;
+
+			const firstTab = tabs[0];
+			const lastTab = tabs[tabs.length - 1];
+
+			const {
+				dataset: { delay: hasDelay },
+			} = currentTabpanel;
+
+			delay = hasDelay !== undefined ? true : false;
+
+			switch (currentKey) {
+				case keys.left:
+					if (!isTab(previousElement)) {
+						activateTab(lastTab, delay);
+					} else {
+						activateTab(previousElement, delay);
+					}
+					event.preventDefault();
+					break;
+				case keys.right:
+					if (!isTab(nextElement)) {
+						activateTab(firstTab, delay);
+					} else {
+						activateTab(nextElement, delay);
+					}
+					event.preventDefault();
+					break;
+				case keys.home:
+					activateTab(firstTab, delay);
+					event.preventDefault();
+					break;
+				case keys.end:
+					activateTab(lastTab, delay);
+					event.preventDefault();
+					break;
+			}
+		}
+
+		function isTab(element) {
+			return element && element.classList.contains(`${tabsetClass}__tab`);
+		}
+
+		function activateTab(element, delay) {
+			if (delay) {
+				setTimeout(function () {
+					element.focus();
+					element.click();
+				}, 300);
+			} else {
+				element.focus();
+				element.click();
+			}
+		}
 	},
 };
